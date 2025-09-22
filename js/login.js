f9d7fac866f26de13b8f6fa0be95a129dd67080e
EmanuelMonella <131817994+EmanuelMonella@users.noreply.github.com>
2025-09-22 15:57:23 -0300

    modificacion y contraseña

--- js/login.js
+++ js/login.js
@@ -1,4 +1,11 @@
-function redirigir(event) {
-    event.preventDefault(); 
-    window.location.href = "pages/index.html"; 
+function redirigir() { 
+    const usuario = document.getElementById('usuario').value;
+    const contrasena = document.getElementById('contrasena').value;
+
+    if (usuario === 'badia' && contrasena === '4038') {
+        window.location.href = "pages/index.html";
+    } else {
+        alert('Usuario o contraseña incorrectos');
+    }
+    
 }
