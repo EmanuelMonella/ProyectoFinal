88413cde0b184c975efd9aebca90b9d23b0a43af
EmanuelMonella <131817994+EmanuelMonella@users.noreply.github.com>
2025-09-27 23:12:29 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ function redirigir() {
     const usuario = document.getElementById('usuario').value;
     const contrasena = document.getElementById('contrasena').value;
 
-    if (usuario === 'badia' && contrasena === '4038') {
+    if (usuario === '' && contrasena === '') {
         window.location.href = "pages/principal.html";
     } else {
         alert('Usuario o contraseña incorrectos');
